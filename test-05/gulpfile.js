5d91482856bc658b15bc17cd4a077fea314f6ec6
Kryshenyk Dmytro <KryshenykDmytro@gmail.com>
2018-12-04 17:55:57 +0200

    gulp practice test

---
+++ test-05/gulpfile.js
@@ -0,0 +1,17 @@
+const gulp = require("gulp");
+const htmlmin = require("gulp-htmlmin");
+const cssnano = require("gulp-cssnano");
+
+gulp.task("htmlminify", function() {
+  return gulp
+    .src("./src/index.html")
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest("./dist"));
+});
+
+gulp.task("cssminify", function() {
+  return gulp
+    .src("./src/css/styles.css")
+    .pipe(cssnano())
+    .pipe(gulp.dest("./dist/css"));
+});

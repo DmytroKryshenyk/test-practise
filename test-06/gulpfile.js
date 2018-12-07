e9cd999b678b06449fb68bad4be265c09bad18bf
Kryshenyk Dmytro <KryshenykDmytro@gmail.com>
2018-12-08 00:41:11 +0200

    added fonts and images to build, added changes to gulpfile.js and packege.json, added normalize.css, created arrow-navigation

--- test-06/gulpfile.js
+++ test-06/gulpfile.js
@@ -1,50 +1,78 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass");
 const htmlmin = require("gulp-htmlmin");
-var cssnano = require('gulp-cssnano');
+// const cssnano = require("gulp-cssnano");
 
 // Таски для збору компіляції SASS в CSS
 gulp.task("sass", function() {
-  return gulp.src(["src/sass/**/*.sass", "src/sass/**/*.scss"])
+  return gulp
+    .src(["src/sass/**/*.sass", "src/sass/**/*.scss"])
     .pipe(sass({ outputStyle: "expanded" }).on("error", sass.logError))
-    .pipe(gulp.dest("src/css"));
+    .pipe(gulp.dest("build/css"));
 });
 
 gulp.task("sass:watch", function() {
   gulp.watch(["src/sass/**/*.sass", "src/sass/**/*.scss"], ["sass"]);
 });
 
+// Таски для мініфікації CSS 
+
+// gulp.task("cssminify", ["sass"], function() {
+//   return gulp
+//     .src("build/css/*.css")
+//     .pipe(cssnano())
+//     .pipe(gulp.dest("build/css"));
+// });
+
+// gulp.task("cssminify:watch", function() {
+//   gulp.watch("build/css/*.css", ["cssminify"]);
+// });
+
+
 // Такси для мініфікації HTML
 gulp.task("htmlminify", () => {
-  return gulp.src("src/*.html")
+  return gulp
+    .src("src/*.html")
     .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(gulp.dest("build"));
 });
 
-gulp.task("htmlminify:watch", function (){
-  gulp.watch("src/*.html", ['htmlminify']);
+gulp.task("htmlminify:watch", function() {
+  gulp.watch("src/*.html", ["htmlminify"]);
 });
 
-// Таски для мініфікації CSS
+// Таск, щоб перемістити картинки
 
-gulp.task('cssminify', ['sass'], function(){
-  return gulp.src('src/css/*.css')
-  .pipe(cssnano())
-  .pipe(gulp.dest('build/css')); 
-}); 
-
-gulp.task("cssminify:watch", function (){
-  gulp.watch("src/css/*.css", ['cssminify']);
+gulp.task("moveimg", function() {
+  return gulp
+  .src("src/img/**/*.{jpg,png,jpeg,svg}")
+  .pipe(gulp.dest("build/img"));
 });
+
+// Таск, щоб перемістити шрифти
+
+gulp.task("movefonts", function() {
+  return gulp
+  .src("src/fonts/**/*.{eot,ttf,woff}")
+  .pipe(gulp.dest("build/fonts"));
+});
+
+// Таск, щоб перемістити вказані нами файли
+gulp.task("movefiles", function() {
+  return gulp
+  .src("src/normalize.css")
+  .pipe(gulp.dest("build"))
+});
+
 
 // Такс для запуску всіх тасків, які ми вкажем
-gulp.task('startall', ['sass', 'cssminify', 'htmlminify']);
+gulp.task("startall", ["sass", "htmlminify", "moveimg", "movefonts", "movefiles"]);
 
-gulp.task('startall:watch', function() {
+gulp.task("startall:watch", function() {
   gulp.watch(["src/sass/**/*.sass", "src/sass/**/*.scss"], ["sass"]);
-  gulp.watch("src/*.html", ['htmlminify']);
-  gulp.watch("src/css/*.css", ['cssminify']);
+  gulp.watch("src/*.html", ["htmlminify"]);
+  // gulp.watch("src/css/*.css", ["cssminify"]);
+  gulp.watch("src/img/**/*.{jpg,png,jpeg,svg}", ["moveimg"]);
+  gulp.watch("src/fonts/**/*.{eot,ttf,woff}", ["movefonts"]);
+  gulp.watch("src/normalize.css", ["movefiles"]);
 });
-
-// gulp.task("startall:watch", gulp.parallel('sass','htmlminify', 'cssminify'));
-

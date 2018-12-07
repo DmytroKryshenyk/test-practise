a5b0032ee4782fb0348c84ba1510d2408d4e01e0
Kryshenyk Dmytro <KryshenykDmytro@gmail.com>
2018-12-07 16:25:38 +0200

    set up an automated assembly and added fonts

--- test-06/gulpfile.js
+++ test-06/gulpfile.js
@@ -1,12 +1,50 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const htmlmin = require("gulp-htmlmin");
+var cssnano = require('gulp-cssnano');
 
-gulp.task('sass', function() {
-  return gulp.src(['sass/**/*.sass', 'sass/**/*.scss'])
-    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-    .pipe(gulp.dest('css'))
+// Таски для збору компіляції SASS в CSS
+gulp.task("sass", function() {
+  return gulp.src(["src/sass/**/*.sass", "src/sass/**/*.scss"])
+    .pipe(sass({ outputStyle: "expanded" }).on("error", sass.logError))
+    .pipe(gulp.dest("src/css"));
 });
 
-gulp.task('sass:watch', function() {
-  gulp.watch(['sass/**/*.sass' , 'sass/**/*.scss'], ['sass']);
+gulp.task("sass:watch", function() {
+  gulp.watch(["src/sass/**/*.sass", "src/sass/**/*.scss"], ["sass"]);
 });
+
+// Такси для мініфікації HTML
+gulp.task("htmlminify", () => {
+  return gulp.src("src/*.html")
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("htmlminify:watch", function (){
+  gulp.watch("src/*.html", ['htmlminify']);
+});
+
+// Таски для мініфікації CSS
+
+gulp.task('cssminify', ['sass'], function(){
+  return gulp.src('src/css/*.css')
+  .pipe(cssnano())
+  .pipe(gulp.dest('build/css')); 
+}); 
+
+gulp.task("cssminify:watch", function (){
+  gulp.watch("src/css/*.css", ['cssminify']);
+});
+
+// Такс для запуску всіх тасків, які ми вкажем
+gulp.task('startall', ['sass', 'cssminify', 'htmlminify']);
+
+gulp.task('startall:watch', function() {
+  gulp.watch(["src/sass/**/*.sass", "src/sass/**/*.scss"], ["sass"]);
+  gulp.watch("src/*.html", ['htmlminify']);
+  gulp.watch("src/css/*.css", ['cssminify']);
+});
+
+// gulp.task("startall:watch", gulp.parallel('sass','htmlminify', 'cssminify'));
+

0246d08b30a7d207c9e57664e8f5b147c3becc7c
Kryshenyk Dmytro <KryshenykDmytro@gmail.com>
2019-08-03 21:48:03 +0300

    vue-lesson-1

---
+++ test-11-vue/src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <p v-if="!isReadyUserData">Заповний форму із своїми контакними данним і добав гостей!</p>
+    <form v-if="!isReadyUserData" @submit.prevent="toggleIsReadyUserData">
+      <label>Email <input type="text" v-model="email" /> </label>
+      <label>First Name <input type="text" v-model="name" /> </label>
+      <label>Last Name <input type="text" v-model="surname" /> </label>
+      <label>Phone <input type="text" v-model="tel" /> </label>
+      <ul v-if="guestsCount > 0">
+        <li v-for="(guest, index) in guests" :key="index">
+          <label> Guest {{index + 1}} <input type="text" v-model="guests[index]" /> </label>
+        </li>
+      </ul>
+      <button type="button" @click="guestsCount += 1; guests.push('')">Add Guests with You</button>
+      <button type="submit" @click="isReadyUserData = !isReadyUserData">Send Data</button>
+    </form>
+
+    <div v-if="isReadyUserData">
+      <h4>All Done!</h4>
+      <p>Email: {{email}}</p>
+      <p>Name: {{name}} {{surname}}</p>
+      <p>Phone: {{tel}}</p>
+      <span>Guests</span>
+      <ul>
+        <li v-for="(guest, index) in guests" :key="index">{{guest}}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      email: "",
+      name: "",
+      surname: "",
+      tel: "",
+      guests: [],
+      guestsCount: 0,
+      isReadyUserData: false
+    };
+  }
+};
+</script>
+
+
+<style lang="scss" scoped>
+input {
+  display: block;
+  width: 25rem;
+}
+
+.submit {
+  display: block;
+  margin: 1rem;
+}
+</style>
+
+
---
+++ test-11-vue/src/components/header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+    <h1>Logo</h1>
+    <p>Це хедер</p>
+  </header>
+</template>
+<script>
+export default {};
+</script>
+
+
+<style lang="scss" scoped>
+h1 {
+  display: inline-block;
+}
+
+p {
+    display: inline-block;
+}
+</style>
+
+
